f7000730b2aa583a73aa2d4941c2fc3625793fbf
CoderBdk <cobdgithub07@gmail.com>
2026-03-29 22:31:39 +0600

    feat: implement QR version specifications and ECC configuration

--- qr-generator/src/main/java/com/coderbdk/qrgenerator/model/ECCLevelSpecs.kt
+++ qr-generator/src/main/java/com/coderbdk/qrgenerator/model/ECCLevelSpecs.kt
@@ -10,7 +10,9 @@ package com.coderbdk.qrgenerator.model
  * @property totalDataCodewords Total number of 8-bit codewords reserved for the actual message.
  * @property errorCorrectionCodewords Number of codewords used for Reed-Solomon error correction.
  * @property maxNumericCapacity Maximum number of digits (0-9) that can be stored in this configuration.
+ * @property maxAlphanumericCapacity Maximum number of characters (0-9, A-Z, etc.) that can be stored.
  * @see <a href="https://www.qrcode.com/en/about/version.html">Official QR Code Capacity Table</a>
+ * @see <a href="https://www.thonky.com/qr-code-tutorial/error-correction-table">Thonky: Error Correction Table</a>
  */
 data class ECCLevelSpecs(
     val level: ECCLevel,
